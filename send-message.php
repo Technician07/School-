7c70553f0ef1cd187564499782738f96e6edf7af
Rin pang <tdene82@gmail.com>
2026-03-15 18:08:22 +0545

    Update send-message.php

--- send-message.php
+++ send-message.php
@@ -1,16 +1,29 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name = htmlspecialchars($_POST['name']);
-    $message = htmlspecialchars($_POST['message']);
-    $to = "nirkunwar6@gmail.com";
-    $subject = "New Message from Your Website";
-    $body = "Name: $name\n\nMessage:\n$message";
-    $headers = "From: noreply@yourdomain.com";
 
-    if(mail($to, $subject, $body, $headers)){
-        echo "<p style='color:lime;text-align:center;margin-top:10px;'>Message sent successfully!</p>";
-    } else {
-        echo "<p style='color:red;text-align:center;margin-top:10px;'>Failed to send message. Try again.</p>";
-    }
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+$to="nirkunwar6@gmail.com";
+$subject="New Message From Batch 2081 Website";
+
+$body="Name: $name\n";
+$body.="Email: $email\n\n";
+$body.="Message:\n$message";
+
+$headers="From: $email";
+
+mail($to,$subject,$body,$headers);
+
+echo "<h2 style='font-family:Arial;text-align:center;margin-top:50px;'>Message Sent Successfully!</h2>";
+echo "<p style='text-align:center;'><a href='index.html'>Go Back to Website</a></p>";
+
 }
+
 ?>
+
+
+
+
